9404c2d42fa33ac3c70dc9fabf5f3e95ea8e12a9
Daniel Macovei <djmacovei@gmail.com>
2024-08-19 13:44:53 -0500

    registration

--- README.md
+++ README.md
@@ -23,7 +23,10 @@ cargo build
 
 Finally, at the moment `wasmatic` expects the same `WASI` flags as `wasmtime`.
 
-`wasmatic up -S common -S http path/to/proxy.wasm`
+`wasmatic up --dir registered::registered -S common -S http path/to/proxy.wasm`
+
+The `--dir` flag in reference to [preopens](https://wa.dev/wasi:filesystem#preopens).
+We're mapping the "registered" folder on the host filesystem to be available to write to using the name "registered", for when we register user-provided handlers.
 
 You can also do some minimal configuration via environment variables.
 
@@ -36,3 +39,14 @@ Setting this variable enables running `wasmatic up` without specifying a path to
 export WASMATIC_PORT=xxxx
 ````
 This will change the port that the operator is running on.
+
+ENDPOINTS
+
+POST /register
+
+Expects a query parameter "name" that is the name of the handler being registered as
+well as a binary to be included in the request body which is the wasm containing the logic
+
+example:
+`curl -X POST "localhost:8080/register?name=foobar" --data-binary "@./task-acl.wasm"`
+write `foobar.wasm` in the "registered" folder on the operator filesystem
--- operator/src/lib.rs
+++ operator/src/lib.rs
@@ -1,3 +1,5 @@
+use std::io::{BufWriter, Write};
+
 use serde::{Deserialize, Serialize};
 #[allow(warnings)]
 mod bindings;
@@ -5,21 +7,44 @@ mod bindings;
 use bindings::exports::wasi::http::incoming_handler::{Guest, IncomingRequest, ResponseOutparam};
 use bindings::wasi::http::types::{Fields, OutgoingBody, OutgoingResponse};
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Body {
-    function: String,
-    key: Option<String>,
-}
 struct Component;
 
 impl Guest for Component {
     fn handle(request: IncomingRequest, response_out: ResponseOutparam) {
+        let method = request.method();
+        match method {
+            bindings::wasi::http::types::Method::Post => {
+                let path = request
+                    .path_with_query()
+                    .expect("Failed to read request path");
+                let mut path_parts = path.split("?");
+                let path = path_parts.next().unwrap();
+
+                if path == "/register" {
+                    let query = path_parts.next().unwrap();
+                    let val = query.split("=").last().unwrap();
+                    let req_body = request.consume().unwrap();
+                    let stream = req_body.stream().unwrap();
+                    let file = std::fs::File::create(format!("./registered/{val}.wasm")).unwrap();
+
+                    let mut more_bytes = true;
+                    let mut write_stream = BufWriter::new(file);
+                    while more_bytes {
+                        let bytes = stream.blocking_read(10000000000000000000);
+                        if let Ok(b) = bytes {
+                            write_stream.write(&b).unwrap();
+                        } else {
+                            more_bytes = false;
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
         let hdrs = Fields::new();
         let resp = OutgoingResponse::new(hdrs);
         let body = resp.body().expect("outgoing response");
         let out = body.write().expect("outgoing stream");
-        // out.blocking_write_and_flush(&res.as_bytes())
-        //     .expect("writing response");
         out.blocking_write_and_flush("hello world".as_bytes())
             .unwrap();
         ResponseOutparam::set(response_out, Ok(resp));
