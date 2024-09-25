5c9fa175361a3bddd6cbd6a92b7332eab0ce64bc
Ethan Frey <ethanfrey@users.noreply.github.com>
2024-09-25 10:44:01 +0200

    Add push, run, and stop docker scripts

---
+++ scripts/push_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eux
+
+# Pushes wasmatic:latest to the GitHub Container Registry
+# Note: if you set TAG environmental variable, it will also tag latest with that tag and push it 
+
+SUDO="sudo"
+if groups | grep -q docker; then
+  SUDO=""
+fi
+
+$SUDO docker push ghcr.io/lay3rlabs/wasmatic:latest
+
+if [ -n "$TAG" ]; then
+  $SUDO docker tag ghcr.io/lay3rlabs/wasmatic:latest ghcr.io/lay3rlabs/wasmatic:$TAG
+  $SUDO docker push ghcr.io/lay3rlabs/wasmatic:$TAG
+fi
