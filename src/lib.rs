999b84834b15b272b781ba3dbde0bee63343034e
Ethan Frey <ethanfrey@users.noreply.github.com>
2024-10-22 14:38:23 +0200

    Move all app code to old/, prepare for rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-//! A CLI for Wasmatic.
-
-//#![deny(missing_docs)]
-
-mod app;
-pub mod commands;
-mod config;
-mod cron_bindings;
-mod digest;
-mod lock;
-mod operator;
-mod queue;
-mod storage;
-mod task_bindings;
+// placeholder to compile
+pub fn add(a: u64, b: u64) -> u64 {
+    a + b
+}
