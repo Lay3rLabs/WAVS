d2927f8044ef2fa70d55554552d61e89ac8b51e7
David Komer <6406986+dakom@users.noreply.github.com>
2024-10-25 11:03:40 +0300

    more config

--- src/config.rs
+++ src/config.rs
@@ -61,11 +61,16 @@ impl ConfigBuilder {
     pub async fn build(self) -> Result<Config> {
         self.load_env()?;
 
+        // first merge the cli and env vars
+        // which has optional values, by default None (or empty)
+        // and parses complex types from strings
         let cli_args: CliArgs = Figment::new()
             .merge(figment::providers::Env::prefixed("MATIC_"))
             .merge(figment::providers::Serialized::defaults(&self.args))
             .extract()?;
 
+        // then, our final config, which can have more complex types with easier TOML-like syntax
+        // and also fills in defaults for required values at the end
         let config: Config = Figment::new()
             .merge(figment::providers::Toml::file(self.filepath()?))
             .merge(figment::providers::Serialized::defaults(cli_args))
@@ -107,23 +112,38 @@ impl ConfigBuilder {
             .cloned()
     }
 
+    /// provides the list of filepaths to try for the config file
     pub fn filepaths_to_try(&self) -> Vec<PathBuf> {
         let mut dirs = Vec::new();
 
+        // explicit arg passed to the cli, e.g. --home /foo
+        // this does not append the default ".wasmatic" subdirectory
+        // instead, it is used as the direct home directory
+        // the final path will then be /foo/wasmatic.toml
         if let Some(dir) = self.args.home.clone() {
             dirs.push(dir);
         }
 
-        // this must be added explicitly, because filepaths is derived
-        // before we merge the cli and env vars
+        // similar to the cli argument, but instead the environment var set like MATIC_HOME="/foo"
+        // this must be added separately from cli arg because at this point cli and env vars are not merged
+        // the final path will then be /foo/wasmatic.toml
         if let Some(dir) = CliArgs::env_var("HOME") {
             dirs.push(dir.into());
         }
 
+        // checks the `.wasmatic/wasmatic.toml` file in the system config directory
+        // this will vary, but the final path with then be something like:
+        // Linux: ~/.config/.wasmatic/wasmatic.toml
+        // macOS: ~/Library/Application Support/.wasmatic/wasmatic.toml
+        // Windows: C:\Users\MyUserName\AppData\Roaming\.wasmatic\wasmatic.toml
         if let Some(dir) = dirs::config_dir().map(|dir| dir.join(Self::DIRNAME)) {
             dirs.push(dir);
         }
 
+        // On linux, this may already be added via config_dir above
+        // but on macOS and windows, and maybe unix-like environments (msys, wsl, etc)
+        // it's helpful to add it explicitly
+        // the final path here typically becomes something like ~/.config/.wasmatic/wasmatic.toml
         if let Some(dir) = std::env::var("XDG_CONFIG_HOME")
             .ok()
             .map(PathBuf::from)
@@ -132,14 +152,23 @@ impl ConfigBuilder {
             dirs.push(dir);
         }
 
+        // Similarly, `config_dir` above may have already added this
+        // but on systems like Windows, it's helpful to add it explicitly
+        // since the system may place the config dir in AppData/Roaming
+        // but we want to check the user's home dir first
+        // this will definitively become something like ~/.config/.wasmatic/wasmatic.toml
         if let Some(dir) = dirs::home_dir().map(|dir| dir.join(".config").join(Self::DIRNAME)) {
             dirs.push(dir);
         }
 
+        // here we want to check the user's home directory directly, not in the `.config` subdirectory
+        // the final path will be something like ~/.wasmatic/wasmatic.toml
         if let Some(dir) = dirs::home_dir().map(|dir| dir.join(Self::DIRNAME)) {
             dirs.push(dir);
         }
 
+        // finally, check the current working directory, wherever the command is run from
+        // i.e. ./wasmatic.toml
         if let Some(dir) = std::env::current_dir()
             .ok()
             .map(|dir| dir.join(Self::DIRNAME))
@@ -147,6 +176,7 @@ impl ConfigBuilder {
             dirs.push(dir);
         }
 
+        // now we have a list of directories to check, we need to add the filename to each
         dirs.into_iter()
             .map(|dir| dir.join(Self::FILENAME))
             .collect()
