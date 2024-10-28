6bb4c78978add18b4838ed3ff2974255920bdb68
Ethan Frey <ethanfrey@users.noreply.github.com>
2024-10-28 12:39:49 +0100

    Add spec for WasmEngine

--- src/apis.rs
+++ src/apis.rs
@@ -8,6 +8,7 @@ use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use tokio::sync::mpsc;
 
+use crate::storage::{CAStorage, CAStorageError};
 use crate::Digest;
 
 /// This is the highest-level container for the system.
@@ -186,6 +187,7 @@ pub enum OperatorError {
 /// ID is meant to identify a component or a service (I don't think we need to enforce the distinction there, do we?)
 /// It is a string, but with some strict validation rules. It must be lowecase alphanumeric: `[a-z0-9-_]{3,32}`
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+#[serde(transparent)]
 pub struct ID(String);
 
 impl ID {
@@ -300,3 +302,56 @@ pub enum TriggerError {
 }
 
 /***** Wasm Engine subsystem *****/
+
+pub struct WasmEngine<S: CAStorage> {
+    wasm_storage: S,
+    // TODO: implement actual wasmtime engine here
+}
+
+// TODO: should we make some trait for quicker tasks where you just register closures for the digests?
+impl<S: CAStorage> WasmEngine<S> {
+    /// Create a new trigger manager.
+    /// This returns the manager and a receiver for the trigger actions.
+    /// Internally, all triggers may run in an async runtime and send results to the receiver.
+    /// Externally, the operator can read the incoming tasks either sync or async
+    pub fn new(wasm_storage: S) -> Self {
+        Self { wasm_storage }
+    }
+
+    pub fn store_wasm(&self, bytecode: &[u8]) -> Result<Digest, WasmEngineError> {
+        // TODO: validate bytecode is proper wasm with some wit interface
+        let digest = self.wasm_storage.set_data(bytecode)?;
+        Ok(digest)
+    }
+
+    // TODO: paginate this
+    pub fn list_digests(&self) -> Result<Vec<Digest>, WasmEngineError> {
+        // TODO: requires a range query on the castorage (.keys())
+        todo!();
+    }
+
+    /// This will execute a contract that implements the layer_avs:task-queue wit interface
+    pub fn execute_queue(
+        &self,
+        _digest: Digest,
+        _request: Vec<u8>,
+        _timestamp: u64,
+    ) -> Result<Vec<u8>, WasmEngineError> {
+        todo!();
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum WasmEngineError {
+    #[error("Storage: {0}")]
+    Storage(#[from] CAStorageError),
+
+    #[error("Invalid Wasm bytecode")]
+    InvalidWasmCode,
+
+    #[error("Wasm doesn't match expected wit interface")]
+    WasmInterfaceMismatch,
+
+    #[error("No wasm found for digest {0}")]
+    UnknownDigest(Digest),
+}
