ab408bce910d7cc2f4ca484565de5d10dbaf223b
Ethan Frey <ethanfrey@users.noreply.github.com>
2024-10-29 20:45:16 +0100

    Basic MockSubmission, but test fails as no way to wait on receipt

--- src/submission/mock.rs
+++ src/submission/mock.rs
@@ -1,38 +1,79 @@
-use tokio::sync::mpsc;
+use std::sync::Arc;
+use tokio::sync::{mpsc, RwLock};
 
-use crate::{
-    apis::submission::{ChainMessage, Submission, SubmissionError},
-    context::AppContext,
-};
+use crate::apis::submission::{ChainMessage, Submission, SubmissionError};
+use crate::context::AppContext;
 
 #[derive(Clone)]
-pub struct MockSubmission {}
+pub struct MockSubmission {
+    inbox: Arc<RwLock<Vec<ChainMessage>>>,
+}
 
 impl MockSubmission {
     #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
-        Self {}
+        Self {
+            inbox: Arc::new(RwLock::new(vec![])),
+        }
+    }
+
+    pub fn received(&self) -> Vec<ChainMessage> {
+        self.inbox.blocking_read().clone()
     }
 }
 
 impl Submission for MockSubmission {
+    // doing this sync so easier to block on
+    // TODO: how to add support for aborting on the kill signal from ctx
+    // (Same on mock triggers)
     fn start(&self, ctx: AppContext) -> Result<mpsc::Sender<ChainMessage>, SubmissionError> {
         let (tx, mut rx) = mpsc::channel(10);
 
+        let mock = self.clone();
         ctx.rt.spawn(async move {
-            loop {
-                match rx.recv().await {
-                    Some(msg) => {
-                        tracing::info!("Received message to submit: {:?}", msg);
-                    }
-                    None => {
-                        tracing::info!("Submission channel closed");
-                        break;
-                    }
-                }
+            while let Some(msg) = rx.recv().await {
+                mock.inbox.write().await.push(msg);
             }
         });
 
         Ok(tx)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::apis::ID;
+
+    use super::*;
+
+    fn dummy_message(service: &str, task_id: u64, payload: &str) -> ChainMessage {
+        ChainMessage {
+            service_id: ID::new(service).unwrap(),
+            workflow_id: ID::new(service).unwrap(),
+            task_id,
+            wasm_result: payload.as_bytes().to_vec(),
+            hd_index: 0,
+            verifier_addr: "verifier".to_string(),
+        }
+    }
+
+    #[test]
+    fn collect_messages() {
+        let submission = MockSubmission::new();
+        assert_eq!(submission.received(), vec![]);
+
+        let ctx = AppContext::new();
+        let send = submission.start(ctx.clone()).unwrap();
+
+        let msg1 = dummy_message("serv1", 1, "foo");
+        let msg2 = dummy_message("serv1", 2, "bar");
+        let msg3 = dummy_message("serv1", 3, "baz");
+
+        send.blocking_send(msg1.clone()).unwrap();
+        assert_eq!(submission.received(), vec![msg1.clone()]);
+
+        send.blocking_send(msg2.clone()).unwrap();
+        send.blocking_send(msg3.clone()).unwrap();
+        assert_eq!(submission.received(), vec![msg1, msg2, msg3]);
     }
 }
