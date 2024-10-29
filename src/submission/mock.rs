ef49f7508b5d565e722cc4c80476e7e9105f920d
Ethan Frey <ethanfrey@users.noreply.github.com>
2024-10-29 20:56:18 +0100

    Add kill switch support to MockSubmission

--- src/submission/mock.rs
+++ src/submission/mock.rs
@@ -30,9 +30,17 @@ impl Submission for MockSubmission {
         let (tx, mut rx) = mpsc::channel(10);
 
         let mock = self.clone();
+        let mut kill_receiver = ctx.get_kill_receiver();
+
         ctx.rt.spawn(async move {
-            while let Some(msg) = rx.recv().await {
-                mock.inbox.write().await.push(msg);
+            loop {
+                tokio::select! {
+                    msg = rx.recv() => match msg {
+                        Some(msg) => mock.inbox.write().await.push(msg),
+                        None => break,
+                    },
+                    _ = kill_receiver.recv() => break,
+                }
             }
         });
 
@@ -81,5 +89,31 @@ mod test {
         // try waiting a bit. is there a way to block somehow?
         sleep(Duration::from_millis(100));
         assert_eq!(submission.received(), vec![msg1, msg2, msg3]);
+    }
+
+    #[test]
+    fn kill_stops_collection() {
+        let submission = MockSubmission::new();
+        assert_eq!(submission.received(), vec![]);
+
+        let ctx = AppContext::new();
+        let send = submission.start(ctx.clone()).unwrap();
+
+        let msg1 = dummy_message("serv2", 11, "foo");
+        let msg2 = dummy_message("serv3", 12, "bar");
+
+        send.blocking_send(msg1.clone()).unwrap();
+        // try waiting a bit. is there a way to block somehow?
+        sleep(Duration::from_millis(100));
+        assert_eq!(submission.received(), vec![msg1.clone()]);
+
+        // now hit the kill switch
+        ctx.kill();
+        sleep(Duration::from_millis(10));
+
+        // future sends should fail
+        send.blocking_send(msg2).unwrap_err();
+        // nothing more should show up
+        assert_eq!(submission.received(), vec![msg1]);
     }
 }
