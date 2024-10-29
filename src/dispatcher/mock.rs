5fd9f83e9d443e894c7cc5016d867efd4a0f1949
Ethan Frey <ethanfrey@users.noreply.github.com>
2024-10-29 18:50:04 +0100

    DispatchManager now implemented for generic Dispatcher

--- src/dispatcher/mock.rs
+++ src/dispatcher/mock.rs
@@ -1,12 +1,11 @@
 use std::sync::Arc;
 
 use crate::{
-    apis::dispatcher::DispatchManager, context::AppContext, engine::WasmEngine,
-    storage::memory::MemoryStorage, submission::mock::MockSubmission,
+    engine::WasmEngine, storage::memory::MemoryStorage, submission::mock::MockSubmission,
     triggers::mock::MockTriggerManager,
 };
 
-use super::{generic::Dispatcher, DispatcherError};
+use super::generic::Dispatcher;
 
 pub type MockDispatcher =
     Dispatcher<MockTriggerManager, Arc<WasmEngine<MemoryStorage>>, MockSubmission>;
@@ -24,39 +23,5 @@ impl MockDispatcher {
         let temp_file = tempfile::NamedTempFile::new().unwrap();
 
         Self::new(triggers, engine, submission, temp_file.as_ref()).unwrap()
-    }
-}
-
-impl DispatchManager for MockDispatcher {
-    type Error = DispatcherError;
-
-    fn start(&self, _ctx: AppContext) -> Result<(), Self::Error> {
-        todo!()
-    }
-
-    fn run_trigger(
-        &self,
-        _action: crate::apis::trigger::TriggerAction,
-    ) -> Result<Option<crate::apis::submission::ChainMessage>, Self::Error> {
-        todo!()
-    }
-
-    fn store_component(
-        &self,
-        _source: crate::apis::dispatcher::WasmSource,
-    ) -> Result<crate::Digest, Self::Error> {
-        todo!()
-    }
-
-    fn add_service(&self, _service: crate::apis::dispatcher::Service) -> Result<(), Self::Error> {
-        todo!()
-    }
-
-    fn remove_service(&self, _id: crate::apis::ID) -> Result<(), Self::Error> {
-        todo!()
-    }
-
-    fn list_services(&self) -> Result<Vec<crate::apis::dispatcher::Service>, Self::Error> {
-        todo!()
     }
 }
