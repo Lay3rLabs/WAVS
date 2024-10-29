054f49908b9be4b2f56b2b385bf3d59e489c6815
Ethan Frey <ethanfrey@users.noreply.github.com>
2024-10-29 22:52:35 +0100

    Test MockEngine

--- src/engine/mock.rs
+++ src/engine/mock.rs
@@ -48,6 +48,7 @@ impl Engine for MockEngine {
         request: Vec<u8>,
         timestamp: u64,
     ) -> Result<Vec<u8>, EngineError> {
+        // FIXME: error if it wasn't stored before as well?
         let store = self.functions.read().unwrap();
         let fx = store
             .get(&digest)
@@ -59,4 +60,63 @@ impl Engine for MockEngine {
 
 pub trait Function: Send + Sync + 'static {
     fn execute(&self, request: Vec<u8>, timestamp: u64) -> Result<Vec<u8>, EngineError>;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn stores_lists() {
+        let engine = MockEngine::new();
+
+        // stores and returns unique digest
+        let d1 = engine.store_wasm(b"foo").unwrap();
+        let d2 = engine.store_wasm(b"bar").unwrap();
+        assert_ne!(d1, d2);
+
+        // list contains both digests (in sorted order)
+        let list = engine.list_digests().unwrap();
+        let mut expected = vec![d1, d2];
+        expected.sort();
+        assert_eq!(list, expected);
+    }
+
+    pub struct FixedResult(Vec<u8>);
+
+    impl Function for FixedResult {
+        fn execute(&self, _request: Vec<u8>, _timestamp: u64) -> Result<Vec<u8>, EngineError> {
+            Ok(self.0.clone())
+        }
+    }
+
+    #[test]
+    fn executes_functions() {
+        let engine = MockEngine::new();
+
+        // stores and returns unique digest
+        let d1 = Digest::new(b"foo");
+        let d2 = Digest::new(b"bar");
+        let d3 = Digest::new(b"missing");
+
+        // register unique handlers for d1 and d2
+        let r1 = b"The first result".to_vec();
+        let r2 = b"HDHFOIUHWEOHGFOEHO".to_vec();
+        engine.register(&d1, FixedResult(r1.clone()));
+        engine.register(&d2, FixedResult(r2.clone()));
+
+        // d1 call gets r1
+        let res = engine.execute_queue(d1, b"123".into(), 1234).unwrap();
+        assert_eq!(res, r1);
+
+        // d2 call gets r2
+        let res = engine.execute_queue(d2, b"123".into(), 1234).unwrap();
+        assert_eq!(res, r2);
+
+        // d1 call returns missing error
+        let err = engine
+            .execute_queue(d3.clone(), b"123".into(), 1234)
+            .unwrap_err();
+        assert!(matches!(err, EngineError::UnknownDigest(_)));
+    }
 }
