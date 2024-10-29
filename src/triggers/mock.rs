51c580ddedaed1eb5c0ed1bb4e076eaad45e350b
Ethan Frey <ethanfrey@users.noreply.github.com>
2024-10-29 19:21:49 +0100

    Mock fixed trigger inputs

--- src/triggers/mock.rs
+++ src/triggers/mock.rs
@@ -1,44 +1,44 @@
-use crate::{
-    apis::trigger::{TriggerAction, TriggerError, TriggerManager},
-    context::AppContext,
-};
+use crate::apis::trigger::{TriggerAction, TriggerData, TriggerError, TriggerManager};
+use crate::apis::ID;
+use crate::context::AppContext;
+
 use tokio::sync::mpsc;
 
 #[derive(Clone)]
-pub struct MockTriggerManager {}
+pub struct MockTriggerManager {
+    triggers: Vec<TriggerAction>, // TODO: add some error conditions
+
+                                  // FIXME: store trigger data for proper list response
+}
 
 impl MockTriggerManager {
-    #[allow(clippy::new_without_default)]
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(triggers: Vec<TriggerAction>) -> Self {
+        Self { triggers }
     }
 }
 
 impl TriggerManager for MockTriggerManager {
     fn start(&self, _ctx: AppContext) -> Result<mpsc::Receiver<TriggerAction>, TriggerError> {
-        todo!()
+        let (sender, receiver) = mpsc::channel(self.triggers.len() + 1);
+        for t in self.triggers.clone() {
+            sender.blocking_send(t);
+        }
+        Ok(receiver)
     }
 
-    fn add_trigger(&self, _trigger: crate::apis::trigger::TriggerData) -> Result<(), TriggerError> {
-        todo!()
+    fn add_trigger(&self, _trigger: TriggerData) -> Result<(), TriggerError> {
+        Ok(())
     }
 
-    fn remove_trigger(
-        &self,
-        _service_id: crate::apis::ID,
-        _workflow_id: crate::apis::ID,
-    ) -> Result<(), TriggerError> {
-        todo!()
+    fn remove_trigger(&self, _service_id: ID, _workflow_id: ID) -> Result<(), TriggerError> {
+        Ok(())
     }
 
-    fn remove_service(&self, _service_id: crate::apis::ID) -> Result<(), TriggerError> {
-        todo!()
+    fn remove_service(&self, _service_id: ID) -> Result<(), TriggerError> {
+        Ok(())
     }
 
-    fn list_triggers(
-        &self,
-        _service_id: crate::apis::ID,
-    ) -> Result<Vec<crate::apis::trigger::TriggerData>, TriggerError> {
-        todo!()
+    fn list_triggers(&self, _service_id: ID) -> Result<Vec<TriggerData>, TriggerError> {
+        Ok(vec![])
     }
 }
