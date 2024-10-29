3a51027b241f4f40f902f684891d825ab70b34dc
Ethan Frey <ethanfrey@users.noreply.github.com>
2024-10-29 20:01:22 +0100

    Implement ID check properly

--- src/apis/types.rs
+++ src/apis/types.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{fmt, ops::Deref};
 
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
@@ -32,7 +32,7 @@ impl ID {
         }
         if !id
             .chars()
-            .all(|c| c.is_ascii_lowercase() && c.is_alphanumeric())
+            .all(|c| c.is_ascii_lowercase() || c.is_numeric() || c == '_' || c == '-')
         {
             return Err(IDError::CharError);
         }
@@ -46,16 +46,70 @@ impl AsRef<str> for ID {
     }
 }
 
+impl Deref for ID {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 impl fmt::Display for ID {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0)
     }
 }
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq, Clone)]
 pub enum IDError {
     #[error("ID must be between 3 and 32 characters")]
     LengthError,
     #[error("ID must be lowercase alphanumeric")]
     CharError,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_ids() {
+        ID::new("foobar").unwrap();
+        ID::new("foot123").unwrap();
+        ID::new("123foot").unwrap();
+        ID::new("two_words").unwrap();
+        ID::new("kebab-case").unwrap();
+        ID::new("pretty-1234321-long").unwrap();
+        // 32 chars
+        ID::new("12345678901234567890123456789012").unwrap();
+    }
+
+    #[test]
+    fn invalid_ids() {
+        // test length
+        let err = ID::new("fo").unwrap_err();
+        assert_eq!(err, IDError::LengthError);
+        let err = ID::new("123456789012345678901234567890123").unwrap_err();
+        assert_eq!(err, IDError::LengthError);
+
+        // test chars
+        let err = ID::new("with space").unwrap_err();
+        assert_eq!(err, IDError::CharError);
+        ID::new("UPPER_SPACE").unwrap_err();
+        ID::new("Capitalized").unwrap_err();
+        ID::new("../../etc/passwd").unwrap_err();
+        ID::new("c:\\\\badfile").unwrap_err();
+    }
+
+    #[test]
+    fn proper_representation() {
+        let name = "fly2you";
+        let id = ID::new(name).unwrap();
+        // same string rep
+        assert_eq!(id.to_string(), name.to_string());
+        // can be used AsRef
+        assert_eq!(name, id.as_ref());
+        // deref working (call method from &str on ID)
+        assert_eq!(name.as_bytes(), id.as_bytes())
+    }
 }
