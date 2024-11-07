972ce194bc11b7a816e0e7beff8c3aaacd96df00
David Komer <6406986+dakom@users.noreply.github.com>
2024-11-07 17:11:34 +0200

    remove comment

--- src/apis/types.rs
+++ src/apis/types.rs
@@ -27,7 +27,6 @@ impl Trigger {
     }
 }
 
-// TODO: custom Deserialize that enforces validation rules
 /// ID is meant to identify a component or a service (I don't think we need to enforce the distinction there, do we?)
 /// It is a string, but with some strict validation rules. It must be lowecase alphanumeric: `[a-z0-9-_]{3,32}`
 #[derive(Serialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
