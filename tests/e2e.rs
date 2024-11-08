664be5ca8bd986afe69e6225532c752ec452f1d5
David Komer <6406986+dakom@users.noreply.github.com>
2024-11-08 11:00:21 +0200

    wip permissions

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -97,7 +97,7 @@ mod e2e {
         let wasm_digest: Digest = match wasm_digest {
             Ok(digest) => digest.parse().unwrap(),
             Err(_) => {
-                let wasm_bytes = include_bytes!("../components/square.wasm");
+                let wasm_bytes = include_bytes!("../components/permissions.wasm");
                 http_client.upload_wasm(wasm_bytes.to_vec()).await.unwrap()
             }
         };
