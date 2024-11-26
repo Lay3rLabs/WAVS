e52abd6c8ec82150ba2285b15ec2f5ba566c2ce2
David Komer <6406986+dakom@users.noreply.github.com>
2024-11-26 18:52:01 +0200

    basic ethereum functionality

---
+++ packages/utils/tests/eth_client.rs
@@ -0,0 +1,57 @@
+use alloy::{network::NetworkWallet, node_bindings::Anvil, providers::Provider, signers::Signer};
+use futures::StreamExt;
+use utils::eth_client::{EthClientBuilder, EthClientConfig};
+
+#[tokio::test]
+async fn client_stream_blocks() {
+    let anvil = Anvil::new().block_time(1).try_spawn().unwrap();
+
+    let config = EthClientConfig {
+        endpoint: anvil.ws_endpoint().to_string(),
+        ..Default::default()
+    };
+
+    let builder = EthClientBuilder::new(config);
+    let client = builder.build_query().await.unwrap();
+
+    let mut stream = client
+        .provider
+        .subscribe_blocks()
+        .await
+        .unwrap()
+        .into_stream();
+
+    let mut counter = 0;
+
+    while counter < 3 {
+        let _header = stream.next().await.unwrap();
+        println!("Received block header: {:?}", _header);
+        counter += 1;
+    }
+}
+
+#[tokio::test]
+async fn client_sign_message() {
+    let anvil = Anvil::new().block_time(1).try_spawn().unwrap();
+
+    let config = EthClientConfig {
+        endpoint: anvil.ws_endpoint().to_string(),
+        mnemonic: Some(
+            "work man father plunge mystery proud hollow address reunion sauce theory bonus"
+                .to_string(),
+        ),
+    };
+
+    let builder = EthClientBuilder::new(config);
+    let client = builder.build_signing().await.unwrap();
+
+    let message = b"hello world";
+
+    // client.wallet doesn't itself allow signing messages, but we created the wallet from the signer
+    let signature = client.signer.sign_message(message).await.unwrap();
+
+    let recovered_address = signature.recover_address_from_msg(&message[..]).unwrap();
+
+    // check that the wallet's default signer is the same as the recovered address
+    assert_eq!(recovered_address, client.wallet.default_signer().address());
+}
