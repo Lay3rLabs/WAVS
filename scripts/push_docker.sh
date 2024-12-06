9f52a6f72413dc439991de8e0675152a4832acef
David Komer <6406986+dakom@users.noreply.github.com>
2024-12-06 11:58:29 +0200

    ci

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-# Pushes wasmatic:latest to the GitHub Container Registry
+# Pushes wavs:latest to the GitHub Container Registry
 # Note: if you set TAG environmental variable, it will also tag latest with that tag and push it 
 
 SUDO="sudo"
@@ -9,10 +9,10 @@ if groups | grep -q docker; then
   SUDO=""
 fi
 
-$SUDO docker push ghcr.io/lay3rlabs/wasmatic:latest
+$SUDO docker push ghcr.io/lay3rlabs/wavs:latest
 
 TAG=${TAG:-}
 if [ -n "$TAG" ]; then
-  $SUDO docker tag ghcr.io/lay3rlabs/wasmatic:latest ghcr.io/lay3rlabs/wasmatic:$TAG
-  $SUDO docker push ghcr.io/lay3rlabs/wasmatic:$TAG
+  $SUDO docker tag ghcr.io/lay3rlabs/wavs:latest ghcr.io/lay3rlabs/wavs:$TAG
+  $SUDO docker push ghcr.io/lay3rlabs/wavs:$TAG
 fi
