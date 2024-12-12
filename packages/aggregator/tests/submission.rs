c067857bba9a8612872a644df448cf383a955f63
Buckram <buckram123@gmail.com>
2024-12-12 21:53:46 +0200

    add quorum 3 test

--- packages/aggregator/tests/submission.rs
+++ packages/aggregator/tests/submission.rs
@@ -62,7 +62,100 @@ async fn submit_to_chain() {
         .unwrap();
 
     let state = HttpState::new((*aggregator.config).clone()).unwrap();
-    let _ = aggregator::http::handlers::service::add_task::add_task(state, request)
+    let response = aggregator::http::handlers::service::add_task::add_task(state, request)
         .await
         .unwrap();
+    assert!(response.hash.is_some())
+}
+
+#[tokio::test]
+async fn submit_to_chain_three() {
+    tracing::info!("Running e2e aggregator");
+    let anvil = Anvil::new().spawn();
+    let aggregator = TestApp::new_with_args(aggregator::args::CliArgs {
+        ws_endpoint: Some(anvil.ws_endpoint()),
+        http_endpoint: Some(anvil.endpoint()),
+        tasks_quorum: Some(3),
+        ..TestApp::default_cli_args()
+    });
+    let eth_client = EthClientBuilder::new(EthClientConfig {
+        ws_endpoint: anvil.ws_endpoint(),
+        http_endpoint: anvil.endpoint(),
+        mnemonic: Some(ANVIL_DEFAULT_MNEMONIC.to_owned()),
+        hd_index: None,
+    })
+    .build_signing()
+    .await
+    .unwrap();
+    let eigen_client = EigenClient::new(eth_client);
+    let core_contracts = eigen_client.deploy_core_contracts().await.unwrap();
+
+    let hello_world_client = HelloWorldFullClientBuilder::new(eigen_client.eth.clone())
+        .avs_addresses(core_contracts.clone())
+        .build()
+        .await
+        .unwrap();
+
+    // Register operator
+    eigen_client
+        .register_operator(&core_contracts)
+        .await
+        .unwrap();
+    hello_world_client
+        .register_operator(&mut OsRng)
+        .await
+        .unwrap();
+
+    let hello_world_client = hello_world_client.into_simple();
+    let state = HttpState::new((*aggregator.config).clone()).unwrap();
+
+    // First we just add task
+    let task_message = "world".to_owned();
+    let NewTaskCreated { task, taskIndex } = hello_world_client
+        .create_new_task(task_message)
+        .await
+        .unwrap();
+    let request = hello_world_client
+        .task_request(task, taskIndex)
+        .await
+        .unwrap();
+
+    let response = aggregator::http::handlers::service::add_task::add_task(state.clone(), request)
+        .await
+        .unwrap();
+    assert!(response.hash.is_none());
+
+    // Second we just add as well
+    let task_message = "bar".to_owned();
+    let NewTaskCreated { task, taskIndex } = hello_world_client
+        .create_new_task(task_message)
+        .await
+        .unwrap();
+
+    let request = hello_world_client
+        .task_request(task, taskIndex)
+        .await
+        .unwrap();
+
+    let response = aggregator::http::handlers::service::add_task::add_task(state.clone(), request)
+        .await
+        .unwrap();
+    assert!(response.hash.is_none());
+
+    // Third should get to the quorum
+    let task_message = "bar".to_owned();
+    let NewTaskCreated { task, taskIndex } = hello_world_client
+        .create_new_task(task_message)
+        .await
+        .unwrap();
+
+    let request = hello_world_client
+        .task_request(task, taskIndex)
+        .await
+        .unwrap();
+
+    let response = aggregator::http::handlers::service::add_task::add_task(state, request)
+        .await
+        .unwrap();
+    assert!(response.hash.is_some());
 }
