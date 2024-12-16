246030af09b0f2cab65b8c77a53159fa73b6ec8b
Buckram <buckram123@gmail.com>
2024-12-16 18:41:27 +0200

    fix submission tests

--- packages/aggregator/src/http/handlers/service/add_service.rs
+++ packages/aggregator/src/http/handlers/service/add_service.rs
@@ -1,12 +1,12 @@
 use axum::{extract::State, response::IntoResponse, Json};
-use utils::eth_client::{AddServiceRequest, AddServiceResponse};
+use utils::hello_world::{AddAggregatorServiceRequest, AddServiceResponse};
 
 use crate::http::{error::HttpResult, state::HttpState};
 
 #[axum::debug_handler]
 pub async fn handle_add_service(
     State(state): State<HttpState>,
-    Json(req): Json<AddServiceRequest>,
+    Json(req): Json<AddAggregatorServiceRequest>,
 ) -> impl IntoResponse {
     match add_service(state, req).await {
         Ok(resp) => Json(resp).into_response(),
@@ -16,7 +16,7 @@ pub async fn handle_add_service(
 
 pub async fn add_service(
     state: HttpState,
-    req: AddServiceRequest,
+    req: AddAggregatorServiceRequest,
 ) -> HttpResult<AddServiceResponse> {
     state.register_service(req.service)?;
     Ok(AddServiceResponse {})
