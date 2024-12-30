a9dbf47181df66d8ab15979b3804d00db13655a8
Reece Williams <reecepbcups@gmail.com>
2024-12-30 16:56:47 -0600

    chore: remove `old` wasmatic directory

--- old/task_bindings.rs
+++
@@ -1,9 +0,0 @@
-use wasmtime::component::bindgen;
-bindgen!({
-  world: "task-queue",
-  async: true,
-  with: {
-      "wasi": wasmtime_wasi::bindings,
-      "wasi:http@0.2.0": wasmtime_wasi_http::bindings::http,
-  },
-});
