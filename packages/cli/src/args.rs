0be22385a12f81036bff2b18364e751fb0703e5f
Reece Williams <reecepbcups@gmail.com>
2025-02-04 16:31:28 -0600

    set to false (I don't think I could override?)

--- packages/cli/src/args.rs
+++ packages/cli/src/args.rs
@@ -50,7 +50,7 @@ pub enum Command {
     /// Uses core contracts that were previously deployed via the CLI
     DeployService {
         /// If set, will register as an operator for the service too
-        #[clap(long, default_value_t = true)]
+        #[clap(long, default_value_t = false)]
         register_operator: bool,
 
         /// Path to the WASI component
