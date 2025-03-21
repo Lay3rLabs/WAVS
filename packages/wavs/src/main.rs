836468f4b37e89e2d989fb6a990245d11666ba1c
Jakub <jakub@moonbite.space>
2025-03-21 15:57:29 +0100

    Provide a function to initialize opentelemetry tracing

--- packages/wavs/src/main.rs
+++ packages/wavs/src/main.rs
@@ -1,6 +1,12 @@
 use std::sync::Arc;
 
 use clap::Parser;
+use opentelemetry::{global, trace::TracerProvider as _};
+use opentelemetry_otlp::{SpanExporter, WithExportConfig};
+use opentelemetry_sdk::{
+    resource::Resource,
+    trace::{self, Sampler, SdkTracerProvider},
+};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use utils::{
     config::{ConfigBuilder, ConfigExt},
@@ -8,20 +14,52 @@ use utils::{
 };
 use wavs::{args::CliArgs, config::Config, dispatcher::CoreDispatcher};
 
+fn setup_tracing(collector: &str) {
+    global::set_text_map_propagator(opentelemetry_jaeger_propagator::Propagator::new());
+    let endpoint = format!("{}/v1/traces", collector);
+    let exporter = SpanExporter::builder()
+        .with_tonic()
+        .with_endpoint(endpoint)
+        .build()
+        .expect("Failed to build OTLP exporter");
+
+    let batch_processor = trace::BatchSpanProcessor::builder(exporter).build();
+
+    let provider = SdkTracerProvider::builder()
+        .with_span_processor(batch_processor)
+        .with_sampler(Sampler::AlwaysOn)
+        .with_resource(Resource::builder().with_service_name("wavs").build())
+        .build();
+    global::set_tracer_provider(provider.clone());
+    let tracer = provider.tracer("readme_example");
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    let subscriber = tracing_subscriber::Registry::default().with(telemetry);
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to set global default subscriber");
+
+    tracing::info!("Jaeger tracing enabled");
+}
+
 fn main() {
     let args = CliArgs::parse();
     let config: Config = ConfigBuilder::new(args).build().unwrap();
 
     // setup tracing
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::fmt::layer()
-                .without_time()
-                .with_target(false),
-        )
-        .with(config.tracing_env_filter().unwrap())
-        .try_init()
-        .unwrap();
+    if let Some(collector) = config.jaeger.as_ref() {
+        setup_tracing(collector);
+    } else {
+        tracing_subscriber::registry()
+            .with(
+                tracing_subscriber::fmt::layer()
+                    .without_time()
+                    .with_target(false),
+            )
+            .with(config.tracing_env_filter().unwrap())
+            .try_init()
+            .unwrap();
+    }
 
     let ctx = AppContext::new();
 
