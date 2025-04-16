3c81f3fe75f6b6a5f8ad8fc2390b4faebd2adb29
Jakub <jakub@moonbite.space>
2025-04-16 14:38:25 +0200

    Remove new instrument decorators due to type incompatibility

--- packages/wavs/src/dispatcher/generic.rs
+++ packages/wavs/src/dispatcher/generic.rs
@@ -313,7 +313,6 @@ impl<T: TriggerManager, E: EngineRunner, S: Submission> DispatchManager for Disp
     }
 }
 
-#[instrument(level = "debug")]
 async fn query_service_from_address(
     chain_name: ChainName,
     address: Address,
@@ -364,7 +363,6 @@ async fn query_service_from_address(
 }
 
 // called at init and when a new service is added
-#[instrument(level = "debug")]
 async fn add_service_to_managers(
     service: Service,
     triggers: &impl TriggerManager,
