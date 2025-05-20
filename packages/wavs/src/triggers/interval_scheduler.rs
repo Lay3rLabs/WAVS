c8ddcb7a922dd45a371cb386a1b120747fbef4bf
David Komer <6406986+dakom@users.noreply.github.com>
2025-05-20 06:13:04 +0300

    fix block scheduler

--- packages/wavs/src/triggers/interval_scheduler.rs
+++ packages/wavs/src/triggers/interval_scheduler.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeMap, HashSet};
 
 use crate::apis::trigger::TriggerError;
 
@@ -8,13 +8,12 @@ use super::core::LookupId;
 // It's used in WAVS for the cron and block interval triggers
 // just need to give it an `impl Interval`
 pub struct IntervalScheduler<T: IntervalTime, S: IntervalState<Time = T>> {
-    // a flat vec so we can quickly iterate over it
-    triggers: Vec<S>,
+    // Key is the next time to run the trigger
+    // and the value is a list of triggers that will run at that time
+    triggers: BTreeMap<T, Vec<S>>,
     unadded_triggers: Vec<S>,
     // just to make sure we don't have duplicates
     trigger_ids: HashSet<LookupId>,
-    // the time from which we kick off the interval loop for each trigger
-    kickoff_time: HashMap<LookupId, T>,
 }
 
 pub trait IntervalTime: Ord + Copy {}
@@ -25,8 +24,16 @@ pub trait IntervalState {
 
     fn lookup_id(&self) -> LookupId;
 
-    // this is usually just `if (now - kickoff_time) % interval == 0`
-    fn interval_hit(&mut self, kickoff_time: Self::Time, now: Self::Time) -> bool;
+    // outer option is whether or not the trigger has hit
+    // inner option is the next time the trigger will hit (if it hasn't ended)
+    fn interval_hit(&mut self, now: Self::Time) -> Option<Option<Self::Time>>;
+
+    // this is called when the trigger is added to the scheduler
+    // it's possible that some time has passed from when the trigger was created
+    // especially in the case of an explicit start_time
+    // so we need to set the kickoff time to the current time and
+    // allow the possibility that the window was missed
+    fn initialize(&mut self, now: Self::Time) -> Option<Self::Time>;
 
     fn start_time(&self) -> Option<Self::Time>;
 
@@ -42,10 +49,9 @@ impl<T: IntervalTime, S: IntervalState<Time = T>> Default for IntervalScheduler<
 impl<T: IntervalTime, S: IntervalState<Time = T>> IntervalScheduler<T, S> {
     pub fn new() -> Self {
         Self {
-            triggers: Vec::new(),
+            triggers: BTreeMap::new(),
             unadded_triggers: Vec::new(),
             trigger_ids: HashSet::new(),
-            kickoff_time: HashMap::new(),
         }
     }
 
@@ -69,62 +75,72 @@ impl<T: IntervalTime, S: IntervalState<Time = T>> IntervalScheduler<T, S> {
         if self.trigger_ids.contains(&id) {
             Ok(false)
         } else {
+            self.trigger_ids.insert(id);
             self.unadded_triggers.push(state);
             Ok(true)
         }
     }
 
-    /// Call this on each “tick”
-    pub fn tick(&mut self, now: T) -> Vec<LookupId> {
+    fn move_unadded_triggers(&mut self, now: T) {
         let mut still_unadded = Vec::new();
         // first add any new triggers whose start time has arrived
         // or, if they don't have a start time, add them immediately
-        for state in self.unadded_triggers.drain(..) {
-            let kickoff_time = match state.start_time() {
-                Some(st) if st > now => {
-                    // hasn't started yet, just keep it and try again next time
+        for mut state in self.unadded_triggers.drain(..) {
+            // if start_time is in the future, hold off…
+            if let Some(st) = state.start_time() {
+                if st > now {
                     still_unadded.push(state);
                     continue;
                 }
-                Some(st) => {
-                    // start time is in the past, so we can add it now
-                    st
-                }
-                None => {
-                    // no start time, make it now
-                    now
-                }
-            };
-
-            self.kickoff_time.insert(state.lookup_id(), kickoff_time);
-            self.triggers.push(state);
+            }
+            // otherwise kick off at the true "now"
+            // even if that's not the exact start time
+            // and it's up to the specific scheduler to manage its
+            // exact interval timing
+            if let Some(next_time) = state.initialize(now) {
+                self.triggers.entry(next_time).or_default().push(state);
+            }
         }
 
         self.unadded_triggers = still_unadded;
+    }
+
+    /// Call this on each “tick”
+    pub fn tick(&mut self, now: T) -> Vec<LookupId> {
+        self.move_unadded_triggers(now);
 
         let mut hits = Vec::new();
-
-        self.triggers.retain_mut(|state| {
-            let kickoff_time = *self.kickoff_time.get(&state.lookup_id()).unwrap();
-            if state.interval_hit(kickoff_time, now) {
-                // this trigger is ready to fire
-                hits.push(state.lookup_id());
+        let mut re_add = Vec::new();
+        while let Some((next_time, _)) = self.triggers.iter().next() {
+            if *next_time > now {
+                break;
             }
+            let (next_time, mut states) = self.triggers.pop_first().unwrap();
+            for mut state in states.drain(..) {
+                let mut re_insert_time = match state.interval_hit(now) {
+                    Some(new_next_time) => {
+                        hits.push(state.lookup_id());
+                        new_next_time
+                    }
+                    None => Some(next_time),
+                };
 
-            // remove the trigger if it has an end time and it's past that time
-            // but only AFTER checking if we hit the interval
-            if let Some(end_time) = state.end_time() {
-                // if the trigger has ended, remove it
-                // we don't remove it from the TriggerManager
-                // since this is more about expirey than full-on removal
-                // and we may still want to look the trigger up by ID
-                // in the manager for debugging etc.
-                if now >= end_time {
-                    return false;
+                if let Some(end_time) = state.end_time() {
+                    if now >= end_time {
+                        re_insert_time = None;
+                    }
+                }
+
+                if let Some(next_time) = re_insert_time {
+                    // if the trigger has a new next time, re-insert it
+                    re_add.push((next_time, state));
                 }
             }
-            true
-        });
+        }
+
+        for (next_time, state) in re_add {
+            self.triggers.entry(next_time).or_default().push(state);
+        }
 
         hits
     }
@@ -133,11 +149,48 @@ impl<T: IntervalTime, S: IntervalState<Time = T>> IntervalScheduler<T, S> {
     pub fn remove_trigger(&mut self, id: LookupId) -> bool {
         let existed = self.trigger_ids.remove(&id);
 
-        self.triggers.retain(|state| state.lookup_id() != id);
+        self.triggers.retain(|_, states| {
+            states.retain(|state| state.lookup_id() != id);
+            !states.is_empty()
+        });
         self.unadded_triggers
             .retain(|state| state.lookup_id() != id);
-        self.kickoff_time.remove(&id);
 
         existed
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Clone)]
+    struct Dummy(LookupId);
+    impl IntervalTime for u32 {}
+    impl IntervalState for Dummy {
+        type Time = u32;
+        fn lookup_id(&self) -> LookupId {
+            self.0
+        }
+        fn interval_hit(&mut self, _now: u32) -> Option<Option<u32>> {
+            None
+        }
+        fn initialize(&mut self, kickoff_time: u32) -> Option<u32> {
+            Some(kickoff_time)
+        }
+        fn start_time(&self) -> Option<u32> {
+            None
+        }
+        fn end_time(&self) -> Option<u32> {
+            None
+        }
+    }
+
+    #[test]
+    fn no_duplicate_adds() {
+        let mut sched = IntervalScheduler::<u32, Dummy>::new();
+        let t1 = Dummy(42);
+        assert_eq!(sched.add_trigger(t1.clone()).unwrap(), true);
+        assert_eq!(sched.add_trigger(t1).unwrap(), false);
     }
 }
