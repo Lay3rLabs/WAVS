2246108d5d104cd1fb171e597569f97a46d78631
David Komer <6406986+dakom@users.noreply.github.com>
2025-07-11 17:00:28 +0300

    consistent naming

--- packages/engine/src/keyvalue/batch.rs
+++ packages/engine/src/keyvalue/batch.rs
@@ -43,7 +43,7 @@ impl batch::Host for KeyValueState<'_> {
 
     fn set_many(
         &mut self,
-        bucket: Resource<batch::Bucket>,
+        bucket: Resource<KeyValueBucket>,
         key_values: Vec<(String, Vec<u8>)>,
     ) -> BatchResult<()> {
         // TODO - try to make db.map_table_write()
@@ -73,7 +73,7 @@ impl batch::Host for KeyValueState<'_> {
 
     fn delete_many(
         &mut self,
-        bucket: Resource<batch::Bucket>,
+        bucket: Resource<KeyValueBucket>,
         keys: Vec<String>,
     ) -> BatchResult<()> {
         // TODO - try to make db.map_table_write()
