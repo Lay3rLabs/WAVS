64ead43fbb33fdafd6c695d33b18f460e972bd86
ismellike <glopez11193@gmail.com>
2025-09-09 15:05:20 -0500

    /packet -> /packets

--- packages/aggregator/src/http/server.rs
+++ packages/aggregator/src/http/server.rs
@@ -59,7 +59,7 @@ pub async fn make_router(config: Config) -> anyhow::Result<axum::Router> {
 
     // protected routes (POSTs)
     let protected = axum::Router::new()
-        .route("/packet", post(handle_packet))
+        .route("/packets", post(handle_packet))
         .route("/services", post(handle_register_service))
         .route("/dev/components", post(handle_upload))
         .with_state(state);
