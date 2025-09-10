4d421006bc49fece1807876fa9964fad20ec2e9d
David Komer <6406986+dakom@users.noreply.github.com>
2025-09-10 16:09:47 +0300

    consolidate deps

--- packages/layer-tests/src/lib.rs
+++ packages/layer-tests/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg(test)] // Everything is test-only
 #![allow(clippy::match_like_matches_macro)]
 #![allow(clippy::match_single_binding)]
 #![allow(clippy::large_enum_variant)]
@@ -10,3 +11,11 @@ pub mod deployment;
 pub mod e2e;
 pub mod example_cosmos_client;
 pub mod example_evm_client;
+
+use crate::args::TestArgs;
+use utils::context::AppContext;
+#[test]
+fn e2e_tests() {
+    let ctx = AppContext::new();
+    e2e::run(TestArgs::default(), ctx);
+}
