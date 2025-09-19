4b2aaa78adb63ce505ddf68b5a344b033fc94787
David Komer <6406986+dakom@users.noreply.github.com>
2025-09-19 17:44:12 +0200

    silence shutdown error

--- packages/wavs/src/main.rs
+++ packages/wavs/src/main.rs
@@ -61,14 +61,15 @@ fn main() {
     let dispatcher = Arc::new(Dispatcher::new(&config_clone, metrics.wavs).unwrap());
 
     wavs::run_server(ctx, config, dispatcher, metrics.http);
+
     if let Some(tracer) = tracer_provider {
-        tracer
-            .shutdown()
-            .expect("TracerProvider should shutdown successfully")
+        if let Err(_) = tracer.shutdown() {
+            //eprintln!("TracerProvider didn't shutdown cleanly: {e:?}")
+        }
     }
     if let Some(meter) = meter_provider {
-        meter
-            .shutdown()
-            .expect("MeterProvider should shutdown successfully")
+        if let Err(_) = meter.shutdown() {
+            //eprintln!("MeterProvider didn't shutdown cleanly: {e:?}")
+        }
     }
 }
