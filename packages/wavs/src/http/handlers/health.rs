f65694eb93795f16650b0a7de0adb6eedb7d046b
Jakub <jakub@moonbite.space>
2025-09-30 13:15:05 +0200

    Perform chain health check on every /health query

--- packages/wavs/src/http/handlers/health.rs
+++ packages/wavs/src/http/handlers/health.rs
@@ -1,7 +1,10 @@
 use axum::{extract::State, http::StatusCode, Json};
 use tracing::instrument;
 
-use crate::{health::HealthStatus, http::state::HttpState};
+use crate::{
+    health::{update_health_status, HealthStatus},
+    http::state::HttpState,
+};
 
 #[utoipa::path(
     get,
@@ -15,6 +18,21 @@ use crate::{health::HealthStatus, http::state::HttpState};
 pub async fn handle_health(
     State(state): State<HttpState>,
 ) -> Result<Json<HealthStatus>, StatusCode> {
+    let chain_configs = state
+        .dispatcher
+        .trigger_manager
+        .chain_configs
+        .read()
+        .unwrap()
+        .clone();
+    let chains = chain_configs
+        .all_chain_keys()
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    update_health_status(&state.health_status, &chain_configs, &chains)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
     let health_status = state.health_status.read().unwrap().clone();
     Ok(Json(health_status))
 }
