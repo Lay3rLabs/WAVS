7514aef7429cab84f0dd47fffe414ddd22bea623
ismellike <glopez11193@gmail.com>
2025-12-04 16:37:00 -0600

    at proto support in echo data

--- examples/components/echo-data/src/lib.rs
+++ examples/components/echo-data/src/lib.rs
@@ -50,7 +50,9 @@ impl Guest for Component {
         }
 
         let (maybe_trigger_id, data) = match trigger_action.data {
-            TriggerData::EvmContractEvent(_) | TriggerData::CosmosContractEvent(_) => {
+            TriggerData::EvmContractEvent(_)
+            | TriggerData::CosmosContractEvent(_)
+            | TriggerData::AtprotoEvent(_) => {
                 let (trigger_id, data) =
                     decode_trigger_event(trigger_action.data).map_err(|e| e.to_string())?;
 
