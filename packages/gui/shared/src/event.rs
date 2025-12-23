d3f46d20b998c5a28f11c56aa914324d1e8daee6
dakom <6406986+dakom@users.noreply.github.com>
2025-12-23 21:07:21 +0200

    forward logs

--- packages/gui/shared/src/event.rs
+++ packages/gui/shared/src/event.rs
@@ -14,3 +14,15 @@ pub struct SettingsEvent {
 impl TauriEventExt for SettingsEvent {
     const NAME: &'static str = "settings";
 }
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LogEvent {
+    pub level: String,
+    pub target: String,
+    pub fields: String,
+}
+
+impl TauriEventExt for LogEvent {
+    const NAME: &'static str = "log";
+}
